cf333f55632d9d8232f3d97b973d8d502b1e4278
José Estuardo Oscal De Leòn <joseoscal89@gmail.com>
2025-05-25 19:52:16 -0600

    13.17

---
+++ Tesla/CybertruchBuy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tesla
+{
+    public partial class CybertruckBuy: Form
+    {
+        public CybertruckBuy()
+        {
+            InitializeComponent();
+        }
+    }
+}
